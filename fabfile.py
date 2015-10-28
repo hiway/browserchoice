de2f6317b52f888531e4e13e662b533ddb589944
Harshad Sharma <harshad@sharma.io>
2015-10-28 18:29:23 +0530

    Don't implode in my face at first install!

--- fabfile.py
+++ fabfile.py
@@ -1,13 +1,18 @@
 from fabric.api import *
 
 
+def remove_old_link():
+    with settings(quiet=True):
+        local('unlink /Users/harshad/bin/browserchoice')
+
+
 def compile():
     local('nuitka --recurse-all --remove-output browserchoice.py')
     local('mv browserchoice.exe browserchoice')
-    local('unlink /Users/harshad/bin/browserchoice')
+    remove_old_link()
     local('ln -s /Users/harshad/dev/browserchoice/browserchoice /Users/harshad/bin/browserchoice')
 
 
 def link():
-    local('unlink /Users/harshad/bin/browserchoice')
+    remove_old_link()
     local('ln -s /Users/harshad/dev/browserchoice/browserchoice.py /Users/harshad/bin/browserchoice')
